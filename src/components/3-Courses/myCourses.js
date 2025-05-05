a8f593403c646f37dd402d0f689c58af9d618f41
Aya Elsherif <96848805+ayaelsherif21@users.noreply.github.com>
2025-05-05 23:12:55 +0300

    Create myCourses.js

---
+++ src/components/3-Courses/myCourses.js
@@ -0,0 +1,13 @@
+export const myProjects = [
+  {CourseName:"Flutter" , InstructorName: "Omar Abdelaziz",Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. " ,category :["cs"] , imgPath:"./images/flutter2.png"},
+  {CourseName:"React" , InstructorName: "Ahmed",Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. "  ,category :["cs"], imgPath:"./images/React2.webp"},
+  {CourseName:"Data Science", InstructorName: "Someone" ,Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. " ,category :["cs"] , imgPath:"./images/d2.png"},
+  // {CourseName:"IOS", InstructorName: "Omar Abdelaziz" ,Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. " ,category :["cs"] , imgPath:"./images/4.jpg"},
+  // {CourseName:"Fundamentals Programming", InstructorName: "Omar Abdelaziz" ,Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. " ,category :["cs"] , imgPath:"./images/3.jpg"},
+  // {CourseName:"Machine Learning", InstructorName: "Omar Abdelaziz" ,Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. " ,category :["cs"] , imgPath:"./images/4.jpg"},
+  /* Art */
+  {CourseName:"Blender 3D Model", InstructorName: "Ahmed Abosaeda" ,Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. ",category :["art"] , imgPath:"./images/blender.jpg"},
+  {CourseName:"Photoshop", InstructorName: "Dai Ashraf" ,Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. " ,category :["art"] , imgPath:"./images/photoshop2.png"},
+  {CourseName:"Modeling Using Maya", InstructorName: "Hajar" ,Details :"Lorem ipsum dolor sit amet consectetur adipisicing elit. " ,category :["art"] , imgPath:"./images/Maya.png"},
+
+]
