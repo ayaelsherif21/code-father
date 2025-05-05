3fe19219dec17b3aafe1c88ba0124e6b7b1a60ba
Aya Elsherif <96848805+ayaelsherif21@users.noreply.github.com>
2025-05-05 23:14:07 +0300

    footer
    added icons

---
+++ src/components/6-Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+export default function Footer() {
+  return (
+    <footer className="flex">
+    
+      <ul className='flex'>
+        <li><a href="">About</a></li>
+        <li><a href="">Courses</a></li>
+        <li><a href="">Team</a></li>
+        <li><a href="">Countact</a></li>
+      </ul>
+      <p>© 2025 Spencer Sharp. All rights reserved.</p>
+    </footer>
+
+  )
+}
